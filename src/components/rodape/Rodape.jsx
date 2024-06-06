b2e378738ff1709565f552bfd91ad590074f9893
LilianDamico <lfonseca_damico@hotmail.com>
2024-06-06 13:06:52 -0300

    first commit

--- src/components/rodape/Rodape.jsx
+++ src/components/rodape/Rodape.jsx
@@ -4,7 +4,7 @@ import './Rodape.css';
 const Rodape = () => {
   return (
     <div className='footer'>
-        <p>&copy; 2024 Shop. All rights reserved. ®</p>
+        <p>&copy; 2024 Shop - seu lugar de compras! All rights reserved. ®</p>
     </div>
   )
 }
